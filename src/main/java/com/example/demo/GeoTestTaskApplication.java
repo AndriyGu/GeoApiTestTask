99f99bf428fd9934f0a7147d897611cf721bd611
AndriyGu <andriy.gumich@gmail.com>
2021-11-06 14:38:32 +0300

    add gitignore

---
+++ src/main/java/com/example/demo/GeoTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoTestTaskApplication.class, args);
+	}
+
+}
